11b78e3691e0329d3227fdbb820dcbac8a9837d8
Jeffrey <okoobohefosa@gmail.com>
2020-05-15 12:29:42 +0100

    revamp code

--- controllers/subjects.js
+++ controllers/subjects.js
@@ -1,154 +1,179 @@
-const Subject = require("../models/subjects")
-const Category = require("../models/category")
+const Subject = require("../models/subjects");
+const Category = require("../models/category");
 
 module.exports = {
-
   // POST categories/:category/sujects
   create: async (req, res) => {
-    const { category } = req.params
-    const { name } = req.body
+    const { category } = req.params;
+    const { name } = req.body;
 
     if (!name) {
       res.status(400).send({
         message: "Invalid field name",
-        documentation_url: "https://github.com/fosajeff/online-tutoring-app-API/blob/master/README.md"
-      })
+        documentation_url:
+          "https://github.com/fosajeff/online-tutoring-app-API/blob/master/README.md"
+      });
     }
 
-    let exist = await Subject.findOne({ name }) && await Category.findOne({ category_name: category})
-    if (!exist) {
-      try {
-        const subject = await Subject.create({ name, category })
-        await subject.save()
+    let checkSubject = await Subject.findOne({ name, category });
+    let checkCategory = await Category.findOne({ category_name: category });
 
-        const categoryByCategoryName = await Category.findOne(
-          { category_name: category }, {_id:0, __v: 0}
-        ).populate("subjects")
-        categoryByCategoryName.subjects.push(subject)
-        await categoryByCategoryName.save()
-        return res.json(categoryByCategoryName)
+    if (!(checkCategory === null) && checkCategory.category_name) {
+      if (checkSubject === null || !checkSubject.name) {
+        try {
+          const subject = await Subject.create({ name, category });
+          await subject.save();
 
-      } catch (err) {console.log(err)}
-
+          const categoryByCategoryName = await Category.findOne({
+            category_name: category
+          }).populate("subjects");
+          categoryByCategoryName.subjects.push(subject);
+          await categoryByCategoryName.save();
+          return res.json(categoryByCategoryName);
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        return res.status(423).send({
+          message: "Subject already exist"
+        });
+      }
+    } else {
+      res.status(423).send({
+        message: "Category does not exist"
+      });
     }
-    res.status(423)
-    .send({
-      message: `Subject or with name ${name} already exist or Category with name ${category} does not exist`,
-    })
   },
 
   // GET /categories/:category/subjects   by category id in req.params object
   subjectsByCategory: async (req, res) => {
-    const { category } = req.params
-    const getCategory = await Category.findOne({ category_name: category }, {_id:0, __v: 0}).populate("subjects")
+    const { category } = req.params;
+    const getCategory = await Category.findOne(
+      { category_name: category },
+      { _id: 0, __v: 0 }
+    ).populate("subjects");
     if (!getCategory) {
-      return res.status(404)
-      .send({
-        message: `Category with name ${category} does not exist`,
-      })
+      return res.status(404).send({
+        message: `Category with name ${category} does not exist`
+      });
     }
-    const subjects = getCategory.subjects
+    const subjects = getCategory.subjects;
     if (!subjects.length > 0) {
-      return res.status(404)
-      .send({
-        message: `No subject exist in the ${category} category`,
-      })
+      return res.status(404).send({
+        message: `No subject exist in the ${category} category`
+      });
     }
-    return res.json(subjects)
+    return res.json(subjects);
   },
 
   // GET /categories/:category/subjects/:id
   subjectByCategory: async (req, res) => {
-    const { category, id } = req.params
-    const getCategory = await Category.findOne({ category_name: category })
+    const { category, id } = req.params;
+    const getCategory = await Category.findOne({ category_name: category });
     if (!getCategory) {
-      res.status(404)
-      .send({
-        message: `Category with name ${category} does not exist`,
-      })
+      res.status(404).send({
+        message: `Category with name ${category} does not exist`
+      });
     }
-    const categoryBySubjects = await Subject.findOne({ name: id, category }, {_id:0, __v: 0})
+    const categoryBySubjects = await Subject.findOne(
+      { name: id, category },
+      { __v: 0 }
+    );
     if (!categoryBySubjects) {
-      res.status(404)
-      .send({
-        message: `No subject with name ${id} exist in the ${category} category`,
-      })
+      res.status(404).send({
+        message: `No subject with name ${id} exist in the ${category} category`
+      });
     }
-    return res.json(categoryBySubjects)
+    return res.json(categoryBySubjects);
   },
-
 
   // GET /subjects?sname=:name
   findAllByQuery: async (req, res) => {
-    const { sname } = req.query
+    const { sname } = req.query;
     if (!sname) {
       res.status(400).send({
         message: "Invalid query parameter",
-        documentation_url: "https://github.com/fosajeff/online-tutoring-app-API/blob/master/README.md"
-      })
+        documentation_url:
+          "https://github.com/fosajeff/online-tutoring-app-API/blob/master/README.md"
+      });
     }
     try {
-      const subjects = await Subject.find({ name: sname }, {_id:0, __v: 0})
+      const subjects = await Subject.find({ name: sname }, { __v: 0 });
       if (!subjects[0]) {
-        return res.status(404)
-        .send({
-          message: `No subject with name ${sname} exist in any category`,
-        })
+        return res.status(404).send({
+          message: `No subject with name ${sname} exist in any category`
+        });
       }
-      return res.json(subjects)
-    } catch (e) { console.log(e) }
+      return res.json(subjects);
+    } catch (e) {
+      console.log(e);
+    }
   },
 
   // Update subject in a category.... [change the name or remove a tutor]
 
   // PUT categories/:category/subjects/:id    // change the name
   updateSubject: async (req, res) => {
-    let { category, id } = req.params
-    let updatedBody = req.body
-    updatedBody.name ? name = updatedBody.name : name = id
-    updatedBody.category ? category = updatedBody.category : category = category
+    const { category, id } = req.params;
+    const updatedBody = req.body;
+    updatedBody.name ? (name = updatedBody.name) : (name = id);
+    updatedBody.category ? (category = updatedBody.category) : category;
 
     try {
-      const getCategory = await Category.findOne({ category_name: category })
-      if (getCategory === null) {
-        return res.status(404)
-        .send({
-          message: `Category with name ${updatedBody.category} does not exist`,
-        })
+      const getCategory = await Category.findOne({ category_name: category });
+      if (getCategory === null || !getCategory.category_name) {
+        return res.status(404).send({
+          message: `Category with name (${updatedBody.category}) does not exist`
+        });
+      } else {
+        const checkSubject = await Subject.findOne({ name: id });
+        if (checkSubject === null || !checkSubject.name) {
+          return res.status(404).send({
+            message: `Subject with name (${id}) does not exist`
+          });
+        } else {
+          // let's update
+          const newSubjectUpdate = await Subject.updateOne(
+            { name: id },
+            { name: name, category: category }
+          );
+          return res.json({
+            message: `${newSubjectUpdate.nModified} subject updated successfully`
+          });
+        }
       }
-    } catch(e) { console.log(e) }
-
-    try {
-      const updateSubjectName = await Subject.findOneAndUpdate({ name: id }, { name:name, category:category })
-      if (updateSubjectName === null) {
-        return res.status(404)
-        .send({
-          message: `Subject with name ${id} does not exist`,
-        })
-      }
-      await updateSubjectName.save()
-      return res.json({message: "Update Successful"})
-    } catch (e) { console.log(e) }
-},
-
+    } catch (e) {
+      console.log(e);
+    }
+  },
 
   // DELETE /categories/:category/subjects/:id
   deleteSubjectByCategory: async (req, res) => {
-    const subject = Subject.findOne({ name: req.params.id })
+    const { id } = req.params;
+    const subject = Subject.findOne({ name: req.params.id });
     if (!subject) {
-      return res.status(404).json({message: `Subject with name ${req.params.id} not found`})
+      return res
+        .status(404)
+        .json({ message: `Subject with name (${req.params.id}) not found` });
     }
     try {
       subject.remove(function(err) {
         if (!err) {
-          Category.update({_id: subject.category}, {$pull: {subject: subject._id}}, function (err, numberAffected) {
-            return res.json({message: `${numberAffected.ok} subject deleted`})
-          })
+          Category.update(
+            { _id: subject.category },
+            { $pull: { subject: subject._id } },
+            function(err, numberAffected) {
+              return res.json({
+                message: `${numberAffected.ok} subject deleted`
+              });
+            }
+          );
         } else {
-          console.log(err);
+          console.error(err);
         }
-      })
-    } catch(e) { console.log(e) }
+      });
+    } catch (e) {
+      console.error(e);
+    }
   }
-
-}
+};
